b01eb09787df4aaa0cd1504b05f8ae47c56f9be5
suzannewetstein <scwetstein@gmail.com>
2019-07-03 13:13:59 +0200

    Update acini_model.py

--- Acini_detection/acini_model.py
+++ Acini_detection/acini_model.py
@@ -23,12 +23,10 @@ def unet(pretrained_weights = None,input_size = (None,None,3)):
     pool3 = MaxPooling2D(pool_size=(2, 2))(conv3)
     conv4 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
     conv4 = Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
-    #drop4 = Dropout(0.5)(conv4)
     pool4 = MaxPooling2D(pool_size=(2, 2))(conv4)
 
     conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool4)
     conv5 = Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv5)
-    #drop5 = Dropout(0.5)(conv5)
 
     up6 = Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(UpSampling2D(size = (2,2))(conv5))
     merge6 = concatenate([conv4,up6], axis = 3)
@@ -49,13 +47,10 @@ def unet(pretrained_weights = None,input_size = (None,None,3)):
     merge9 = concatenate([conv1,up9], axis = 3)
     conv9 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = Conv2D(32, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
-    #conv9 = Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv10 = Conv2D(1, 1, activation = 'sigmoid')(conv9)
     model = Model(input = inputs, outputs = conv10)
 
     model.compile(optimizer = Adam(lr = 1e-4), loss = 'binary_crossentropy', metrics = ['mean_squared_error'])
-    
-    #model.summary()
 
     if(pretrained_weights):
     	model.load_weights(pretrained_weights)
